dd78f59da49236c2bae5a1643bd5c9c6a19c1309
igorbardic <73275227+igorbardic@users.noreply.github.com>
2020-11-02 09:57:09 +0100

    Add files via upload

--- sql_queries.py
+++ sql_queries.py
@@ -21,9 +21,9 @@ time_table_create = ("CREATE TABLE IF NOT EXISTS time (start_time timestamp PRIM
 
 # INSERT RECORDS
 
-songplay_table_insert = ("INSERT INTO songplays (start_time , user_id , level , song_id , artist_id , session_id , location , user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET level = 'paid'")
+songplay_table_insert = ("INSERT INTO songplays (start_time , user_id , level , song_id , artist_id , session_id , location , user_agent) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ON CONFLICT (songplay_id) DO NOTHING")
                         
-user_table_insert = ("INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO NOTHING")
+user_table_insert = ("INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE set level = EXCLUDED.level;")
                      
 song_table_insert = ("INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (song_id) DO NOTHING")
 
